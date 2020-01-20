13d50ec1d286318c124d87edbeeda814a0eab3fd
Pragya Shrestha <pragyasresta@gmail.com>
2020-01-20 13:50:29 +0545

    Delete config file

--- src/config.py
+++
@@ -1,7 +0,0 @@
-
-#moneytun
-MONEYTUN_CONFIG = {
-    'BASE_URL': 'https://webservices-moneytun.staging.wwcny.com',
-    'ACCESS_TOKEN': 'MTS98235-CDBD-42F8-MNET-4B4F6C060API',
-    'SECRET_TOKEN': 'lpzpg33nEMhDDjJWEsUxItyHmHSVdyHRUA4t9hWEiSs='
-}
